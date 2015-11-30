788bf61a91f9d061c42767715deaeb422907da53
Antoine Burnel <burnel.antoine@gmail.com>
2015-11-30 09:26:00 +0100

    commit 30.11

--- app/controllers/TachesController.php
+++ app/controllers/TachesController.php
@@ -21,15 +21,61 @@ class TachesController extends DefaultController {
 		parent::frmAction ( $id );
 	}
 	public function showAction($id = NULL) {
-		$taches = call_user_func ( $this->model . "::find", array (
-				"id=" . $id 
-		) );
-		
-		$this->view->setVars ( array (
-				"taches" => $taches,
-				"siteUrl" => $this->url->getBaseUri (),
-				"baseHref" => $this->dispatcher->getControllerName () 
-		) );
+		if ($this->request->isAjax ()) {
+			$user = user::find ();
+			$taches = call_user_func ( $this->model . "::find", array (
+					"id=" . $id 
+			) );
+			
+			$PoidsDev = array ();
+			$IdDev = array ();
+			$NomDev = array ();
+			$color = array ();
+			
+			$PoidTotal = 0;
+			$colorDev = 0;
+			
+			foreach ( $taches as $ta ) {
+				$IdDev [$ta->getIdDev ()] = $ta->getIdDev ();
+				$PoidTotal = $PoidTotal + $ta->getPoids ();
+			}
+			
+			foreach ( $IdDev as $name ) {
+				foreach ( $taches as $t ) {
+					if ($name == $t->getId ()) {
+						$NomDev [$t->getIdentite ()] = $t->getIdentite ();
+						$usecaseDev = call_user_func ( "usecase::find", array (
+								"idProjet=" . $id,
+								"idDev=" . $NomDev [$t->getIdentite ()] 
+						) );
+						foreach ( $usecaseDev as $ucD ) {
+							if ($ucD->getIdDev () == $t->getId ()) {
+								$PoidsDev [$NomDev [$t->getIdentite ()]] += $ucD->getPoids ();
+							}
+						}
+						$PoidsDev [$NomDev [$t->getIdentite ()]] = floor ( ($PoidsDev [$NomDev [$t->getIdentite ()]] / $PoidTotal) * 100 ) . "%";
+						if ($colorDev == 0) {
+							$color [$NomDev [$t->getIdentite ()]] = "#E2ECFF";
+							$colorDev = 1;
+						} else {
+							$color [$NomDev [$t->getIdentite ()]] = "#A3C0FF";
+							$colorDev = 0;
+						}
+					}
+				}
+			}
+			
+			$this->view->setVars ( array (
+					"taches" => $taches,
+					"siteUrl" => $this->url->getBaseUri (),
+					"baseHref" => $this->dispatcher->getControllerName (),
+					"dev" => $NomDev,
+					"poid" => $PoidsDev,
+					"color" => $color 
+			) );
+		} else {
+			throw new Exception ( "404 not found", 1 );
+		}
 		
 		parent::frmAction ( $id );
 	}
