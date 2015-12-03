ee9e3a92f4cbc0581b8cc32980d9b371c838e7a8
aclavel <antoine.clavel@sts-sio-caen.info>
2015-12-03 10:48:23 +0100

    MAJ BDD OMFG

--- database/increase.sql
+++ database/increase.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.2.11
+-- version 4.2.7.1
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 09 Novembre 2015 à 01:12
--- Version du serveur :  5.6.21
--- Version de PHP :  5.6.3
+-- Généré le :  Jeu 03 Décembre 2015 à 10:36
+-- Version du serveur :  5.5.39
+-- Version de PHP :  5.4.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -21,6 +21,48 @@ SET time_zone = "+00:00";
 --
 CREATE DATABASE IF NOT EXISTS `increase` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 USE `increase`;
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `acl`
+--
+
+CREATE TABLE IF NOT EXISTS `acl` (
+`id` int(11) NOT NULL,
+  `idRole` int(11) NOT NULL,
+  `nom_ressource` varchar(50) NOT NULL,
+  `nom_action` varchar(50) NOT NULL,
+  `allowed` tinyint(1) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+
+--
+-- Contenu de la table `acl`
+--
+
+INSERT INTO `acl` (`id`, `idRole`, `nom_ressource`, `nom_action`, `allowed`) VALUES
+(3, 1, 'index', 'index', 1),
+(4, 1, 'messages', 'update', 0),
+(5, 1, 'messages', 'index', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `action`
+--
+
+CREATE TABLE IF NOT EXISTS `action` (
+  `nom_action` varchar(50) NOT NULL,
+  `nom_ressource` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `action`
+--
+
+INSERT INTO `action` (`nom_action`, `nom_ressource`) VALUES
+('index', 'index'),
+('index', 'messages'),
+('update', 'messages');
 
 -- --------------------------------------------------------
 
@@ -36,7 +78,7 @@ CREATE TABLE IF NOT EXISTS `message` (
   `idUser` int(11) NOT NULL,
   `idProjet` int(11) NOT NULL,
   `idFil` int(11) DEFAULT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Contenu de la table `message`
@@ -60,16 +102,55 @@ CREATE TABLE IF NOT EXISTS `projet` (
   `dateFinPrevue` date DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `idClient` int(11) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
 
 --
 -- Contenu de la table `projet`
 --
 
 INSERT INTO `projet` (`id`, `nom`, `description`, `dateLancement`, `dateFinPrevue`, `image`, `idClient`) VALUES
-(1, 'Increase', 'A Phalcon web application to manage the progress of projects, and improve communication with the customer', '2015-03-16', '2016-03-29', NULL, 1),
-(2, 'Open-beer', 'A free, public database, API and web application for beer information.', '2015-03-15', '2016-03-29', NULL, 1),
-(3, 'Essai', 'test&lt;html&gt;la suite', '2015-03-10', '2016-03-09', NULL, 1);
+(1, 'Increase', 'A Phalcon web application to manage the progress of projects, and improve communication with the customer', '2015-03-16', '2015-03-29', NULL, 1),
+(2, 'Open-beer', 'A free, public database, API and web application for beer information.', '2015-03-15', '2015-03-29', NULL, 1),
+(3, 'Essai', 'test&lt;html&gt;la suite', '2015-03-10', '2015-03-09', NULL, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `ressource`
+--
+
+CREATE TABLE IF NOT EXISTS `ressource` (
+  `nom` varchar(50) NOT NULL,
+  `description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `ressource`
+--
+
+INSERT INTO `ressource` (`nom`, `description`) VALUES
+('index', 'index'),
+('messages', '');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+CREATE TABLE IF NOT EXISTS `role` (
+`id` int(11) NOT NULL,
+  `libelle` varchar(50) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `role`
+--
+
+INSERT INTO `role` (`id`, `libelle`) VALUES
+(1, 'user'),
+(2, 'author'),
+(3, 'admin');
 
 -- --------------------------------------------------------
 
@@ -83,7 +164,7 @@ CREATE TABLE IF NOT EXISTS `tache` (
   `date` date DEFAULT NULL,
   `avancement` smallint(6) DEFAULT NULL,
   `codeUseCase` varchar(15) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
 
 --
 -- Contenu de la table `tache`
@@ -146,24 +227,36 @@ CREATE TABLE IF NOT EXISTS `user` (
   `mail` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   `identite` varchar(100) DEFAULT NULL,
-  `role` varchar(255) NOT NULL DEFAULT 'user'
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+  `idRole` int(11) NOT NULL
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
 
 --
 -- Contenu de la table `user`
 --
 
-INSERT INTO `user` (`id`, `mail`, `password`, `identite`, `role`) VALUES
-(1, 'jcheron@kobject.net', 'ffffff9afffffff15b336e6affffff9619ffffff92ffffff8537ffffffdf30ffffffb2ffffffe6ffffffa2376569fffffffcfffffff9ffffffd7ffffffe773ffffffecffffffceffffffde65606529ffffffa0', 'JC HERON', 'user'),
-(2, 'igor.minar@gmail.com', 'ffffff9afffffff15b336e6affffff9619ffffff92ffffff8537ffffffdf30ffffffb2ffffffe6ffffffa2376569fffffffcfffffff9ffffffd7ffffffe773ffffffecffffffceffffffde65606529ffffffa0', 'Igor MINAR', 'author'),
-(3, 'admin@local.fr', 'ffffff8c6976ffffffe5ffffffb5410415ffffffbdffffffe908ffffffbd4dffffffee15ffffffdfffffffb167ffffffa9ffffffc873fffffffc4bffffffb8ffffffa81f6f2affffffb448ffffffa918', 'Admin', 'admin,user,author'),
-(4, 'misko.hevery@gmail.com', '15ffffffe2ffffffb0ffffffd3ffffffc338ffffff91ffffffebffffffb0fffffff1ffffffef60ffffff9effffffc419420c20ffffffe320ffffffceffffff94ffffffc65fffffffbcffffff8c331244ffffff8effffffb225', 'Miško Hevery', 'author'),
-(5, 'pete.bacon@gmail.com', '15ffffffe2ffffffb0ffffffd3ffffffc338ffffff91ffffffebffffffb0fffffff1ffffffef60ffffff9effffffc419420c20ffffffe320ffffffceffffff94ffffffc65fffffffbcffffff8c331244ffffff8effffffb225', 'Pete Bacon Darwin', 'author'),
-(7, 'moi@kobject.net', 'ffffff9afffffff15b336e6affffff9619ffffff92ffffff8537ffffffdf30ffffffb2ffffffe6ffffffa2376569fffffffcfffffff9ffffffd7ffffffe773ffffffecffffffceffffffde65606529ffffffa0', 'moi', 'user');
+INSERT INTO `user` (`id`, `mail`, `password`, `identite`, `idRole`) VALUES
+(1, 'jcheron@kobject.net', 'ffffff9afffffff15b336e6affffff9619ffffff92ffffff8537ffffffdf30ffffffb2ffffffe6ffffffa2376569fffffffcfffffff9ffffffd7ffffffe773ffffffecffffffceffffffde65606529ffffffa0', 'JC HERON', 1),
+(2, 'igor.minar@gmail.com', 'ffffff9afffffff15b336e6affffff9619ffffff92ffffff8537ffffffdf30ffffffb2ffffffe6ffffffa2376569fffffffcfffffff9ffffffd7ffffffe773ffffffecffffffceffffffde65606529ffffffa0', 'Igor MINAR', 1),
+(4, 'misko.hevery@gmail.com', 'ffffff9afffffff15b336e6affffff9619ffffff92ffffff8537ffffffdf30ffffffb2ffffffe6ffffffa2376569fffffffcfffffff9ffffffd7ffffffe773ffffffecffffffceffffffde65606529ffffffa0', 'Miško Hevery', 2),
+(5, 'pete.bacon@gmail.com', 'ffffff9afffffff15b336e6affffff9619ffffff92ffffff8537ffffffdf30ffffffb2ffffffe6ffffffa2376569fffffffcfffffff9ffffffd7ffffffe773ffffffecffffffceffffffde65606529ffffffa0', 'Pete Bacon Darwin', 2),
+(7, 'moi@kobject.net', 'ffffff9afffffff15b336e6affffff9619ffffff92ffffff8537ffffffdf30ffffffb2ffffffe6ffffffa2376569fffffffcfffffff9ffffffd7ffffffe773ffffffecffffffceffffffde65606529ffffffa0', 'moi', 3),
+(10, 'admin@gmail.com', 'ffffff9afffffff15b336e6affffff9619ffffff92ffffff8537ffffffdf30ffffffb2ffffffe6ffffffa2376569fffffffcfffffff9ffffffd7ffffffe773ffffffecffffffceffffffde65606529ffffffa0', 'admin', 3);
 
 --
 -- Index pour les tables exportées
 --
+
+--
+-- Index pour la table `acl`
+--
+ALTER TABLE `acl`
+ ADD PRIMARY KEY (`id`), ADD KEY `nom_ressource` (`nom_ressource`), ADD KEY `nom_action` (`nom_action`), ADD KEY `nom_action_2` (`nom_action`), ADD KEY `idRole` (`idRole`);
+
+--
+-- Index pour la table `action`
+--
+ALTER TABLE `action`
+ ADD PRIMARY KEY (`nom_action`,`nom_ressource`), ADD KEY `nom_ressource` (`nom_ressource`);
 
 --
 -- Index pour la table `message`
@@ -176,6 +269,18 @@ ALTER TABLE `message`
 --
 ALTER TABLE `projet`
  ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `nom_UNIQUE` (`nom`), ADD KEY `fk_projet_user1_idx` (`idClient`);
+
+--
+-- Index pour la table `ressource`
+--
+ALTER TABLE `ressource`
+ ADD PRIMARY KEY (`nom`);
+
+--
+-- Index pour la table `role`
+--
+ALTER TABLE `role`
+ ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `tache`
@@ -193,12 +298,17 @@ ALTER TABLE `usecase`
 -- Index pour la table `user`
 --
 ALTER TABLE `user`
- ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `mail_UNIQUE` (`mail`);
+ ADD PRIMARY KEY (`id`), ADD UNIQUE KEY `mail_UNIQUE` (`mail`), ADD KEY `idRole` (`idRole`);
 
 --
 -- AUTO_INCREMENT pour les tables exportées
 --
 
+--
+-- AUTO_INCREMENT pour la table `acl`
+--
+ALTER TABLE `acl`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=6;
 --
 -- AUTO_INCREMENT pour la table `message`
 --
@@ -210,6 +320,11 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 ALTER TABLE `projet`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT pour la table `role`
+--
+ALTER TABLE `role`
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=4;
+--
 -- AUTO_INCREMENT pour la table `tache`
 --
 ALTER TABLE `tache`
@@ -218,10 +333,24 @@ MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=8;
+MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=11;
 --
 -- Contraintes pour les tables exportées
 --
+
+--
+-- Contraintes pour la table `acl`
+--
+ALTER TABLE `acl`
+ADD CONSTRAINT `acl_ibfk_1` FOREIGN KEY (`nom_ressource`) REFERENCES `ressource` (`nom`),
+ADD CONSTRAINT `acl_ibfk_2` FOREIGN KEY (`nom_action`) REFERENCES `action` (`nom_action`),
+ADD CONSTRAINT `acl_ibfk_3` FOREIGN KEY (`idRole`) REFERENCES `role` (`id`);
+
+--
+-- Contraintes pour la table `action`
+--
+ALTER TABLE `action`
+ADD CONSTRAINT `action_ibfk_1` FOREIGN KEY (`nom_ressource`) REFERENCES `ressource` (`nom`);
 
 --
 -- Contraintes pour la table `message`
@@ -249,6 +378,12 @@ ADD CONSTRAINT `fk_tache_useCase1` FOREIGN KEY (`codeUseCase`) REFERENCES `useca
 ALTER TABLE `usecase`
 ADD CONSTRAINT `fk_useCase_projet` FOREIGN KEY (`idProjet`) REFERENCES `projet` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
 ADD CONSTRAINT `fk_useCase_user1` FOREIGN KEY (`idDev`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `user`
+--
+ALTER TABLE `user`
+ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`idRole`) REFERENCES `role` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
